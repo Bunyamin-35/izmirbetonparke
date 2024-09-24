d7d554c1b9bfa2b297e2f5aab8dfae426dbbd86b
Bunyamin-35 <bunyaminbulut.ce@gmail.com>
2024-09-24 22:40:14 +0300

    izmirbetonparke.com.tr | pages created.

---
+++ app/blog/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SinglePostPge = () => {
+  return (
+    <div>SinglePostPge</div>
+  )
+}
+
+export default SinglePostPge
---
+++ app/data/nav.js
@@ -0,0 +1,14 @@
+const nav = [
+    {
+        id:1,
+        link:'/',
+        name: 'home',
+        active:true,
+    },
+    {
+        id:2,
+        link:'/about',
+        name: 'about',
+        active:true,
+    },
+]
