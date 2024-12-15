bd14d4d59a47c5103118b861181577988e4d332e
Bunyamin Bulut <bunyaminbulut.ce@gmail.com>
2024-12-15 21:16:53 +0300

    izmirbetonparke.com | next script added.

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 
+import Script from 'next/script';
 
 
 import localFont from "next/font/local";
@@ -28,8 +29,8 @@ const geistMono = localFont({
 
 export const metadata = {
   title: {
-    default:"Ana Sayfa -İzmir Beton Parke",
-    template:"%s - İzmir Beton Parke"
+    default: "Ana Sayfa -İzmir Beton Parke",
+    template: "%s - İzmir Beton Parke"
   },
   description: "Balaban Sirketler Grubu",
 };
@@ -43,16 +44,14 @@ export default function RootLayout({ children }) {
         <div
           className="container"
         >
-          
+
           <Navbar />
           {children}
           <Footer />
         </div>
-
-
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"
-        ></script>
+        ></Script>
       </body>
     </html>
   );
