c9783fecc6dc3c5e5f59c3885d936c6b49ba7fa1
Bunyamin Bulut <bunyaminbulut.ce@gmail.com>
2024-12-22 20:30:20 +0300

    izmirbetonparke.com.tr |footer dark mode canceled.

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const FooterItem = ({ text, link }) => {
 const FooterBlockItem = ({ title, items }) => {
   return (
     <div className="space-y-6">
-      <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-100">
+      <h1 className="text-lg font-semibold text-gray-900 ">
         {title}
       </h1>
       <ul className="space-y-3">
