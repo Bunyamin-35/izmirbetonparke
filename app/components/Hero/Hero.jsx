2dd3aefb127f79abdb9bd580dd5987ddc22d069c
Bunyamin Bulut <bunyaminbulut.ce@gmail.com>
2024-12-22 20:33:46 +0300

    izmirbetonparke.com.tr | hero dark mode canceled.

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -29,13 +29,13 @@ export default function HeroSection() {
             <div className="relative mx-auto lg:max-w-7xl w-full px-5 sm:px-10 md:px-12 lg:px-5 grid lg:grid-cols-2 lg:items-center gap-10 z-10">
                 <div className="flex flex-col space-y-8 sm:space-y-10 lg:items-center text-center lg:text-left max-w-2xl md:max-w-3xl mx-auto">
                     <div className="relative bg-white bg-opacity-80 border-8 p-5 border-gray-200 rounded-xl">
-                        <h1 className="font-semibold leading-tight text-teal-950 dark:text-white text-4xl sm:text-5xl lg:text-6xl">
+                        <h1 className="font-semibold leading-tight text-teal-950  text-4xl sm:text-5xl lg:text-6xl">
                             40 yılı aşkın tecrübe{" "}
                             <span className="text-transparent bg-clip-text bg-gradient-to-tr from-pink-700 to-orange-800">
                                 Beton Parke Taşı
                             </span>
                         </h1>
-                        <p className="text-gray-700 dark:text-gray-300 tracking-tight md:font-normal max-w-xl mx-auto lg:max-w-none hidden md:block my-2">
+                        <p className="text-gray-700 tracking-tight md:font-normal max-w-xl mx-auto lg:max-w-none hidden md:block my-2">
                             Dayanıklı ve estetik beton parke taşlarıyla projelerinize değer
                             katıyoruz. 40 yılı aşkın deneyimimizle kaliteli üretim ve
                             profesyonel uygulamalar sunuyoruz.
@@ -52,7 +52,7 @@ export default function HeroSection() {
                     </div>
                 </div>
                 <div className="flex aspect-square lg:aspect-auto lg:h-[35rem] relative">
-                    <div className="w-3/5 h-[80%] rounded-3xl overflow-clip border-8 border-gray-200 dark:border-gray-950 z-30">
+                    <div className="w-3/5 h-[80%] rounded-3xl overflow-clip border-8 border-gray-200  z-30">
                         <Image
                             src={hero1}
                             alt="buildind plan image"
@@ -61,7 +61,7 @@ export default function HeroSection() {
                             className="w-full h-full object-cover z-30"
                         />
                     </div>
-                    <div className="absolute right-0 bottom-0 h-[calc(100%-50px)] w-4/5 rounded-3xl overflow-clip border-4 border-gray-200 dark:border-gray-800 z-10">
+                    <div className="absolute right-0 bottom-0 h-[calc(100%-50px)] w-4/5 rounded-3xl overflow-clip border-4 border-gray-200 0 z-10">
                         <Image
                             src={hero2}
                             alt="working-on-housing-project"
